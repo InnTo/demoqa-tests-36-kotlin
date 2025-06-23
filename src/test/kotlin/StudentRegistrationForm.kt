bef7c8662b606a5fa2e8ef3ca0d984a037f6c0ff
Inna Torukalo <iniska2002@gmail.com>
2025-06-23 13:52:07 +0300

    refactoring for Kotlin in StudentRegistrationForm

--- src/test/kotlin/StudentRegistrationForm.kt
+++ src/test/kotlin/StudentRegistrationForm.kt
@@ -1,7 +1,7 @@
-import com.codeborne.selenide.Condition
+import com.codeborne.selenide.Condition.*
 import com.codeborne.selenide.Configuration
-import com.codeborne.selenide.Selectors
-import com.codeborne.selenide.Selenide
+import com.codeborne.selenide.Selectors.*
+import com.codeborne.selenide.Selenide.*
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 
@@ -19,45 +19,45 @@ class StudentRegistrationForm {
 
     @Test
     fun fillRegistrationFormTest() {
-        Selenide.open("/automation-practice-form")
+        open("/automation-practice-form")
 
         //Заполнение формы
-        Selenide.`$`("#firstName").setValue("Test")
-        Selenide.`$`("#lastName").setValue("TestLastName")
-        Selenide.`$`("#userEmail").setValue("Test@test.ru")
-        Selenide.`$`(Selectors.byText("Male")).click()
-        Selenide.`$`("#userNumber").setValue("8961530834")
+        element("#firstName").setValue("Test")
+        element("#lastName").setValue("TestLastName")
+        element("#userEmail").setValue("Test@test.ru")
+        element(byText("Male")).click()
+        element("#userNumber").setValue("8961530834")
 
-        Selenide.`$`("#dateOfBirthInput").clear()
-        Selenide.`$`(".react-datepicker__month-select").selectOption("April")
-        Selenide.`$`(".react-datepicker__year-select").selectOption("2002")
-        Selenide.`$$`(".react-datepicker__day").findBy(Condition.text("30")).click()
+        element("#dateOfBirthInput").clear()
+        element(".react-datepicker__month-select").selectOption("April")
+        element(".react-datepicker__year-select").selectOption("2002")
+        elements(".react-datepicker__day").findBy(text("30")).click()
 
-        Selenide.`$`("#subjectsInput").setValue("Maths")
-        Selenide.`$$`(".subjects-auto-complete__option").findBy(Condition.text("Maths")).click() //. указывает, что это класс, а не тег, можно явно прописывать класс
+        element("#subjectsInput").setValue("Maths")
+        elements(".subjects-auto-complete__option").findBy(text("Maths")).click() //. указывает, что это класс, а не тег, можно явно прописывать класс
 
 
-        Selenide.`$`(Selectors.byText("Sports")).click()
-        Selenide.`$`("#uploadPicture").uploadFromClasspath("test.jpg")
-        Selenide.`$`("#currentAddress").setValue("Current test address")
+        element(byText("Sports")).click()
+        element("#uploadPicture").uploadFromClasspath("test.jpg")
+        element("#currentAddress").setValue("Current test address")
 
-        Selenide.`$`("#state").click()
-        Selenide.`$`(Selectors.byText("Uttar Pradesh")).click()
-        Selenide.`$`("#city").click()
-        Selenide.`$`(Selectors.byText("Agra")).click()
+        element("#state").click()
+        element(byText("Uttar Pradesh")).click()
+        element("#city").click()
+        element(byText("Agra")).click()
 
-        Selenide.`$`("#submit").click()
+        element("#submit").click()
 
         //Проверка заполнения
-        Selenide.`$`(".table-responsive").shouldHave(Condition.text("Test TestLastName"))
-        Selenide.`$`(".table-responsive").shouldHave(Condition.text("Test@test.ru"))
-        Selenide.`$`(".table-responsive").shouldHave(Condition.text("Male"))
-        Selenide.`$`(".table-responsive").shouldHave(Condition.text("8961530834"))
-        Selenide.`$`(".table-responsive").shouldHave(Condition.text("30 April,2002"))
-        Selenide.`$`(".table-responsive").shouldHave(Condition.text("Maths"))
-        Selenide.`$`(".table-responsive").shouldHave(Condition.text("Sports"))
-        Selenide.`$`(".table-responsive").shouldHave(Condition.text("test.jpg"))
-        Selenide.`$`(".table-responsive").shouldHave(Condition.text("Current test address"))
-        Selenide.`$`(".table-responsive").shouldHave(Condition.text("Uttar Pradesh Agra"))
+        element(".table-responsive").shouldHave(text("Test TestLastName"))
+        element(".table-responsive").shouldHave(text("Test@test.ru"))
+        element(".table-responsive").shouldHave(text("Male"))
+        element(".table-responsive").shouldHave(text("8961530834"))
+        element(".table-responsive").shouldHave(text("30 April,2002"))
+        element(".table-responsive").shouldHave(text("Maths"))
+        element(".table-responsive").shouldHave(text("Sports"))
+        element(".table-responsive").shouldHave(text("test.jpg"))
+        element(".table-responsive").shouldHave(text("Current test address"))
+        element(".table-responsive").shouldHave(text("Uttar Pradesh Agra"))
     }
 }
